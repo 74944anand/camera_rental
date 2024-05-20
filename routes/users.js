f4f78566fb61c2c76a0ff08cae3890607a8e3ef4
74944anand <anandkole9@gmail.com>
2024-05-20 19:15:45 +0530

    user module completed

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require("express");
-const { getUserById, addUser } = require("../controllers/user");
+const {
+  getUserById,
+  addUser,
+  getAllUser,
+  updateUserById,
+  deleteUserById,
+  updateUserPassword,
+} = require("../controllers/user");
 const { checkAuth } = require("../utils/auth");
 var router = express.Router();
 
 router.get("/:id", checkAuth, getUserById);
 router.post("/", addUser);
+router.get("/", checkAuth, getAllUser);
+router.put("/:id", checkAuth, updateUserById);
+router.delete("/:id", checkAuth, deleteUserById);
+router.put("/:id", checkAuth, updateUserPassword);
 
 module.exports = router;
